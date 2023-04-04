e0f4c1b140686cdcc83c8f17aa636371af375fc8
markothedev <markothedevmail@gmail.com>
2023-04-04 22:00:57 +0100

    added more config,third party api and made folder structure

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,18 @@
+import './Login.style.scss'
+
+
+const Login =():React.ReactElement=>{
+
+    return (
+        <div className='auth_container'>
+            <div className="auth_img_container">
+                <img src="" alt="" />
+            </div>
+            <div className="auth_form_container">
+                <p>Enter details to login.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
